a5a6ce67ab9c790837b5a25b482b7d3cdce8c1ba
Axia <mcvoidredstone@gmail.com>
2026-04-06 23:21:29 +0100

    fixed #20 lmao that was so deceptively easy

--- common/src/main/java/com/ordana/dimensional_tears/mixins/dimensional_tears/BoatPaddleSoundMixin.java
+++ common/src/main/java/com/ordana/dimensional_tears/mixins/dimensional_tears/BoatPaddleSoundMixin.java
@@ -1,6 +1,7 @@
 package com.ordana.dimensional_tears.mixins.dimensional_tears;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.ordana.dimensional_tears.fluids.DimensionalTearsFluid;
 import com.ordana.dimensional_tears.reg.ModSoundEvents;
 import net.minecraft.sounds.SoundEvent;
@@ -8,31 +9,24 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.Level;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Boat.class)
 public abstract class BoatPaddleSoundMixin extends Entity {
 
     @Shadow private double waterLevel;
-    @Unique private boolean dimensional_tears$rowingInDimTears = false;
 
     public BoatPaddleSoundMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
-    @ModifyExpressionValue(method = "getStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/vehicle/Boat;checkInWater()Z"))
-    private boolean shouldRegardAsInWater(boolean original) {
-        this.dimensional_tears$rowingInDimTears = DimensionalTearsFluid.isBoatRowingIn(this.level(), this.getBoundingBox(), () -> this.waterLevel, d -> this.waterLevel = d);
-        return original || this.dimensional_tears$rowingInDimTears;
-    }
+    @WrapMethod(method = "getPaddleSound")
+    private SoundEvent directDimTearsSoundEvent(Operation<SoundEvent> original) {
+        if (DimensionalTearsFluid.isBoatRowingIn(this.level(), this.getBoundingBox(), () -> this.waterLevel, d -> this.waterLevel = d))
+            return ModSoundEvents.BOAT_PADDLE_DIMENSIONAL_TEARS.get();
 
-    @ModifyExpressionValue(method = "getPaddleSound", at = @At(value = "FIELD", target = "Lnet/minecraft/sounds/SoundEvents;BOAT_PADDLE_WATER:Lnet/minecraft/sounds/SoundEvent;", opcode = Opcodes.GETSTATIC))
-    private SoundEvent directDimTearsSoundEvent(SoundEvent original) {
-        return this.dimensional_tears$rowingInDimTears ? ModSoundEvents.BOAT_PADDLE_DIMENSIONAL_TEARS.get() : original;
+        return original.call();
     }
 
 }
